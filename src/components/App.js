f66aeea324282db1f837a3c7d686cfa0ce8d4ce2
Ashishkalbande11 <ashishkalbande6@gmail.com>
2024-10-22 15:53:56 +0530

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,2 +1,13 @@
-<p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+import React from 'react'
 
+const App = () => {
+  return (
+    <div>
+        <p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+
+        <h1>hehe</h1>
+    </div>
+  )
+}
+
+export default App
